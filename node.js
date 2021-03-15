a6d9fa67d08ebacced9d69f9e89496d5fa915f3f
pearpoem <pearpoem@naver.com>
2021-03-15 22:51:39 +0900

    node test5

--- node.js
+++ node.js
@@ -5,9 +5,15 @@ const Upbit = require('./upbit_lib')
 const timeout = ms => new Promise(res => setTimeout(res, ms))
 const upbit = new Upbit('1', '1')
 
+let notiJson;
+
 app.get("/", (req, res) => {
-    let notiJson;
-    notiJson  = await upbit.proxy_test()
     res.send(notiJson)
     })
 app.listen(PORT)
+
+
+
+async function start() {
+    notiJson  = await upbit.proxy_test();
+}
